fa649cd2977f09e2906c2c617949cfb7428e4aad
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-24 22:23:24 +0600

    testing project

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       {/* main section start */}
       <main>
         <h1 className="text-center text-5xl font-bold">Welcome to my world</h1>
+        <h1 className="text-center text-5xl font-bold">Welcome to my world</h1>
       </main>
       {/* footer sectiob start */}
       <footer></footer>
