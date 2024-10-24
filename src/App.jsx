6d35a7a9f735e28b237c299f431c9eced28d819c
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-24 22:22:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* header section  start*/}
+      <header>
+        {/* nav section  */}
+        {/* banner section */}
+      </header>
+      {/* main section start */}
+      <main>
+        <h1 className="text-center text-5xl font-bold">Welcome to my world</h1>
+      </main>
+      {/* footer sectiob start */}
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
